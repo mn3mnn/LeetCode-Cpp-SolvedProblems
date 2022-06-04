f08ca3594f466f26fe5120958f0ec81e7c41144a
Monaem Tarek <abdoeladawy4@gmail.com>
2022-06-04 19:49:46 +0200

    Time: 1380 ms (5.01%), Space: 10.2 MB (67.97%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
+    /* 
     vector<int> res;
     for (int i=0; i < nums.size(); i++)
     {
@@ -20,5 +21,34 @@ public:
         }
     }
     return res;
+    */
+        
+        
+        
+    // another solution 
+    //    O(n * lg(n))
+         
+        
+    // store values and the indices in map
+    unordered_map<int,int> mp;    
+		
+    // iterate through nums vector
+    for(int i = 0; i < nums.size(); i++){
+        
+		if(mp.find(target-nums[i]) != mp.end()){
+            
+        // if mp contains the value (target - nums[i]), then  
+        // return the indices
+            return {i, mp[target-nums[i]]};
+        }			
+        
+        mp[nums[i]] = i;
+        
+    }
+        
+    // else return an empty vector.    
+    return {};
+   
 }
+
 };
