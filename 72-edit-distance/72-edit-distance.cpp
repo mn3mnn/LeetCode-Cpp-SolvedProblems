8e6da80577a4d8909c99b64535394cede9a44ae2
Monem Tarek <abdoeladawy4@gmail.com>
2022-08-04 23:01:26 +0200

    Time: 3 ms (99.45%), Space: 7.1 MB (89.45%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -4,11 +4,13 @@ public:
         return std::min(std::min(a, b), c);
     }
     
-    int minDistance(string str1, string str2) {
-        
+   int minDistance(string str1, string str2){
+       
+    int gapCost = 1, mismatchCost = 1; 
+
     int len1 = str1.length();
     int len2 = str2.length();
-        
+
     // table of costs
     int DP [len1 + 1][len2 + 1];
 
@@ -17,36 +19,30 @@ public:
 
             // If first string is empty, the only option is to
             // insert all characters of the other string
-            // number of ops = the len of the other string
+            // number of ops (cost) = the len of the other string
             if (i == 0){
-                DP[i][j] = j;
+                DP[i][j] = j * gapCost;
             }
 
             // If second string is empty, the only option is to
             // insert all characters of the other string
-            // number of ops = the len of the other string
+            // number of ops (cost) = the len of the other string
             else if (j == 0)
             {
-                DP[i][j] = i;
+                DP[i][j] = i * gapCost;
             }
 
-            // If last characters are same, ignore last chars
-            // and recur for remaining string
-            else if (str1[i - 1] == str2[j - 1]){
-                DP[i][j] = DP[i - 1][j - 1];
-            }
-
-            // If the last character is different, consider
-            // all possibilities and find the minimum
             else {
-                DP[i][j] = 1 + min(DP[i - 1][j - 1],// diagonal up left (replace)
-                                   DP[i - 1][j], // up (remove)
-                                   DP[i][j - 1]); // left (insert)
+                // 0 cost for match
+                int misCost = (str1[i - 1] == str2[j - 1] ? 0 : mismatchCost);
+
+                DP[i][j] = min(DP[i - 1][j - 1] + misCost,// diagonal (replace)
+                               DP[i - 1][j] + gapCost, // (delete)
+                               DP[i][j - 1] + gapCost); // (insert)
             }
         }
     }
 
     return DP[len1][len2];
-        
-    }
+}
 };
