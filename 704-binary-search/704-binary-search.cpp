f739a1c22ae7ed95eee548353d81753464d3721e
Monem Tarek <abdoeladawy4@gmail.com>
2022-08-16 16:22:38 +0200

    Time: 81 ms (13.00%), Space: 27.6 MB (12.32%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+       while(left <= right){
+            
+            int m = left + (right - left) / 2;
+            
+            if(nums[m] == target){
+                return m;
+            }
+            else if(nums[m] > target){
+                right = m - 1;
+            }
+            else{
+                left = m + 1;
+            }
+        }
+        return -1;
+        
+    }
+};
