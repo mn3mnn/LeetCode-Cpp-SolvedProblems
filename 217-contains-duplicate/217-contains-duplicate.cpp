7b16800f9320ad6b3be53339453297b1f9bb869a
Monem Tarek <abdoeladawy4@gmail.com>
2022-09-13 11:29:20 +0200

    Time: 198 ms (20.16%), Space: 46.6 MB (92.00%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int sz = nums.size() - 1;
         std::sort(nums.begin(), nums.end());
-        for(int i = 0; i < sz; ++i){
+        for(int i = 0; i < nums.size() - 1; ++i){
             if (nums[i] == nums[i + 1]){
                 return true;
             }
         }
         return false;
-
+        
     }
 };
