81eb2b466df66270a6b8959f45974769dac93a12
Monem Tarek <abdoeladawy4@gmail.com>
2022-09-14 13:13:52 +0200

    Time: 129 ms (85.12%), Space: 70.5 MB (6.11%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -11,11 +11,11 @@ public:
         dp[0] = nums[0];
 
         for (int i = 1; i < sz; ++i) {
-        //  dp[i] = max(nums[i], dp[i - 1] + nums[i]);
-            dp[i] = (nums[i] > dp[i - 1] + nums[i] ? nums[i] : dp[i - 1] + nums[i]);
+            dp[i] = max(nums[i], dp[i - 1] + nums[i]);
+        //  dp[i] = (nums[i] > dp[i - 1] + nums[i] ? nums[i] : dp[i - 1] + nums[i]);
         }
 
-        return *max_element(dp.begin(), dp.end());;
+        return *max_element(dp.begin(), dp.end());
     }
 
 };
