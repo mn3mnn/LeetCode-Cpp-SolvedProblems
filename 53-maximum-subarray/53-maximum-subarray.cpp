498521da683e8fa119a87f4b1899eee66c3f7feb
Monem Tarek <abdoeladawy4@gmail.com>
2022-09-14 13:06:44 +0200

    Time: 290 ms (5.22%), Space: 70.5 MB (6.11%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,24 +4,23 @@ class Solution {
 public:
 
 
-
     int maxSubArray(vector<int>& nums) {
         int sz = nums.size();
-
         if (sz == 1) return nums[0];
 
         vector<int> dp(sz, 0);
         dp[0] = nums[0];
+        int max_sum = dp[0];
 
         int j = 0;
         for (int i = 1; i < sz; ++i) {
+    //        dp[i] = max(nums[i], dp[j] + nums[i]);
             dp[i] = (nums[i] > dp[j] + nums[i] ? nums[i] : dp[j] + nums[i]);
-            // dp[i] = max(nums[i], dp[j] + nums[i]);
             j++;
+            max_sum = (dp[i] > max_sum ? dp[i] : max_sum);
         }
-
-        return *max_element(dp.begin(), dp.end());
+    //    int max_sum = *max_element(dp.begin(), dp.end());
+        return max_sum;
     }
-
 
 };
