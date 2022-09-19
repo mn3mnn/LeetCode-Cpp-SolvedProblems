fb06e02f6b8d3175432a6397fcec088e9814457d
Monem Tarek <abdoeladawy4@gmail.com>
2022-09-19 16:20:51 +0200

    Time: 22 ms (27.96%), Space: 7.5 MB (9.40%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    bool isAnagram(string s, string t) {
+
+        if (s.size() != t.size()){
+            return false;
+        }
+
+        map<char, int> mp1;
+        for (int i = 0; i < s.size(); ++i) {
+            mp1[s[i]]++;
+        }
+        map<char, int> mp2;
+        for (int i = 0; i < t.size(); ++i) {
+            mp2[t[i]]++;
+        }
+        for (int i = 0; i < s.size(); ++i) {
+            if (mp1[s[i]] != mp2[s[i]]){
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
