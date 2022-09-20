b3a4e528475e40dad34f8a1d07024ed0afef9eba
Monem Tarek <abdoeladawy4@gmail.com>
2022-09-20 13:54:01 +0200

    Time: 22 ms (34.94%), Space: 8.1 MB (17.92%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -10,29 +10,46 @@
 class Solution {
 public:
 
-
-
     bool hasCycle(ListNode *head) {
 
         if (head == NULL || head->next==NULL){
             return false;
         }
 
-        map<ListNode*, int> mp;
-    //    vector<ListNode*> vec;
-    //    vec.push_back(NULL);
-        ListNode *tmp = head;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
 
-        while (tmp->next != NULL){
-            mp[tmp]++;
-            if (mp[tmp] > 1){
+        while(fast != NULL && fast->next != NULL){
+            if (slow == fast){
                 return true;
             }
-    //        vec.push_back(tmp);
-            tmp = tmp->next;
-        }
 
+            slow = slow->next;
+            fast = fast->next->next;
+        }
         return false;
+
     }
+
+
+//     bool hasCycle(ListNode *head) {
+
+//         if (head == NULL || head->next==NULL){
+//             return false;
+//         }
+
+//         map<ListNode*, int> mp;
+//         ListNode *tmp = head;
+
+//         while (tmp->next != NULL){
+//             mp[tmp]++;
+//             if (mp[tmp] > 1){
+//                 return true;
+//             }
+//             tmp = tmp->next;
+//         }
+
+//         return false;
+//     }
 };
 
